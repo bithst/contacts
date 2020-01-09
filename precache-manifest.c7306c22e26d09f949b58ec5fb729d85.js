f9cc739825115991e4d22ea319b2886bfee03fa3
Bishal Thapa Shrestha <bishalthapashrestha@gmail.com>
2020-01-09 13:45:00 +0545

    Updates

---
+++ precache-manifest.c7306c22e26d09f949b58ec5fb729d85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d197ed66eaae9719c4f68104dd5809",
+    "url": "/contact/index.html"
+  },
+  {
+    "revision": "5d095ea7dfdf0090f536",
+    "url": "/contact/static/css/main.83e92af1.chunk.css"
+  },
+  {
+    "revision": "f190367339afc96db1d3",
+    "url": "/contact/static/js/2.c10d0fae.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/contact/static/js/2.c10d0fae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d095ea7dfdf0090f536",
+    "url": "/contact/static/js/main.7267ba3b.chunk.js"
+  },
+  {
+    "revision": "37a47e500a17697ba81c",
+    "url": "/contact/static/js/runtime-main.9cfa8762.js"
+  }
+]);
